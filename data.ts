444b5a047f6889bde252da0bc5a5a5bc82b79bd4
Jefferson Silva <i4097@rn.senac.local>
2024-08-27 09:07:44 -0300

    loja-basica

---
+++ data.ts
@@ -0,0 +1,35 @@
+import { Product } from "./types/Product"
+
+//ESSA LISTA VAI SER UMA ARRAY DE PRODUCT
+export const list: Product[] = [
+    {
+        id: 1,
+        image: "https://img.lojasrenner.com.br/item/914448135/original/3.jpg",
+        name: "Camiseta em Tecido",
+        price: 197.58,
+    },
+    {
+        id: 2,
+        image: "https://img.lojasrenner.com.br/item/927418445/original/3.jpg",
+        name: "Calça Slim Cropped",
+        price: 179,
+    },
+    {
+        id: 3,
+        image: "https://img.lojasrenner.com.br/item/927066929/original/12.jpg",
+        name: "Camiseta Boxy",
+        price: 89,
+    },
+    {
+        id:4,
+        image: "https://img.lojasrenner.com.br/item/927373929/original/3.jpg",
+        name: "Camiseta New",
+        price: 99,
+    },
+    {
+        id: 5,
+        image: "https://img.lojasrenner.com.br/item/897669728/original/3.jpg",
+        name: "Jaqueta Trucker",
+        price: 299,
+    },
+]
